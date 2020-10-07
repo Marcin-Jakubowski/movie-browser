31cd195528a51288f96b7dca87001ca0efe3f55d
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-07 11:44:11 +0200

    Updates

---
+++ precache-manifest.47489f2c57ad79271fc6c254133a2ab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e5fea48d70fdd1cc19293db2ba3b98",
+    "url": "/movie-browser/index.html"
+  },
+  {
+    "revision": "dc57ec3e225669bcf2c3",
+    "url": "/movie-browser/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "721702aac5b133a993d0",
+    "url": "/movie-browser/static/js/2.0f2b97c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-browser/static/js/2.0f2b97c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc57ec3e225669bcf2c3",
+    "url": "/movie-browser/static/js/main.fcd608c5.chunk.js"
+  },
+  {
+    "revision": "940b2a428110daf5a21e",
+    "url": "/movie-browser/static/js/runtime-main.6da0a001.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-browser/static/media/logo.5d5d9eef.svg"
+  }
+]);
