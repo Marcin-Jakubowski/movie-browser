621f26911721d9e1143c985a406ec652e9127d73
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-07 19:14:38 +0200

    formatting code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Movies from "./Movies/index";
 import People from "./People/index";
 import { StyledNavLink, NavList, NavListItem } from "./styled";
 
 export default () => (
-    <HashRouter>
-        <nav>
-            <NavList>
-                <NavListItem>
-                    <StyledNavLink to="/movies">Movies</StyledNavLink>
-                </NavListItem>
-                <NavListItem>
-                    <StyledNavLink to="/people">People</StyledNavLink>
-                </NavListItem>
-            </NavList>
-            <Switch>
-                <Route path="/movies">
-                    <Movies />
-                </Route>
-                <Route path="/people">
-                    <People />
-                </Route>
-            </Switch>
-        </nav>
-    </HashRouter>
+  <HashRouter>
+    <nav>
+      <NavList>
+        <NavListItem>
+          <StyledNavLink to="/movies">Movies</StyledNavLink>
+        </NavListItem>
+        <NavListItem>
+          <StyledNavLink to="/people">People</StyledNavLink>
+        </NavListItem>
+      </NavList>
+      <Switch>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/people">
+          <People />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
 );
