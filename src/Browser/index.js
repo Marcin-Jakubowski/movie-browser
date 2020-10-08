79319347f477826838f15c61735f056e0e4c0a30
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-09 01:46:31 +0200

    adding Browser component

---
+++ src/Browser/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserInput } from "./styled"
+
+function Browser() {
+
+    return (
+        <BrowserInput
+            placeholder="Search for movies..."
+        />
+    );
+};
+
+export default Browser;
