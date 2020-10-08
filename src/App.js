bba0aa974a0f2d7acf1d9f34e85fd52cda73d4b0
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-09 01:47:04 +0200

    styling navigation

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Movies from "./Movies/index";
 import People from "./People/index";
-import { StyledNavLink, NavList, NavListItem } from "./styled";
+import NavLogoButton from "./NavLogoButton/index";
+import Browser from "./Browser/index";
+import { StyledNavLink, StyledNavLinkLogo, NavList, NavListItem, NavContainer } from "./styled";
 
 export default () => (
   <HashRouter>
     <nav>
       <NavList>
-        <NavListItem>
-          <StyledNavLink to="/movies">MOVIES</StyledNavLink>
-        </NavListItem>
-        <NavListItem>
-          <StyledNavLink to="/people">PEOPLE</StyledNavLink>
-        </NavListItem>
+        <NavContainer>
+          <NavListItem>
+            <StyledNavLinkLogo to="/movies"><NavLogoButton /></StyledNavLinkLogo>
+          </NavListItem>
+          <NavListItem>
+            <StyledNavLink to="/movies">MOVIES</StyledNavLink>
+          </NavListItem>
+          <NavListItem>
+            <StyledNavLink to="/people">PEOPLE</StyledNavLink>
+          </NavListItem>
+          <Browser />
+        </NavContainer>
       </NavList>
       <Switch>
         <Route path="/movies">
--- src/styled.js
+++ src/styled.js
@@ -7,8 +7,9 @@ export const StyledNavLink = styled(NavLink).attrs(() => ({
     activeClassName,
 }))`
     text-decoration: none;
+    font-weight: 600;
     color: white;
-    padding: 13.5px;
+    padding: 13.5px 20px;
     
     &.${activeClassName} {
         border: 1px solid white;
@@ -19,20 +20,38 @@ export const StyledNavLink = styled(NavLink).attrs(() => ({
     }
 `;
 
+export const StyledNavLinkLogo = styled(NavLink).attrs(() => ({
+    activeClassName,
+}))`
+    text-decoration: none;
+    color: white;
+    padding: 13.5px;
+
+    &:link {
+        color: white
+    }
+`;
+
 export const NavListItem = styled.li`
-    padding: 10px 30px;
+    padding: 10px 0px;
 `;
 
 export const NavList = styled.ul`
-    text-decoration: none;
     list-style: none;
     background-color: black;
     display: flex;
-    flex-direction: row;
-    justify-content: center;
     align-items: center;
     margin: 0;
     height: 94px;
     font-size: 14px;
     line-height: 21px;
+`;
+
+export const NavContainer = styled.div`
+    width: 1368px;
+    display: grid;
+    grid-template-columns: repeat(3, auto) 1fr;
+    grid-gap: 10px;
+    align-items: center;
+    margin: 0 auto;
 `;
