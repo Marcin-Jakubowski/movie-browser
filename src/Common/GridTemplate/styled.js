6ae9a28729c994cae471103fe3e47fae2f8f8f49
Pfeszler <pfeszler@gmail.com>
2020-10-08 22:00:07 +0200

    fixed grid template hight

--- src/Common/GridTemplate/styled.js
+++ src/Common/GridTemplate/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 export const Wrapper = styled.section`
     width: 100%;
+    min-height: 1391px;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: 650px;
