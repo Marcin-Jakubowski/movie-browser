2b045279623d865d53b3a1680668f4506c658e7d
Pfeszler <pfeszler@gmail.com>
2020-10-14 10:19:46 +0200

    Modified movies component to display search results when needed

--- src/Movies/index.js
+++ src/Movies/index.js
@@ -6,23 +6,26 @@ import Header from '../Common/Header';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectSelectedPage, setSelectedPage } from './MoviesSlice';
 import Pager from '../Common/Pager';
+import { useSearch } from './useSearch';
+import useQueryParameter from '../useQueryParameter';
 
 
 function Movies() {
 
   const dispatch = useDispatch()
+  const query = useQueryParameter()
 
   const page = useSelector(selectSelectedPage)
-
   useEffect(() => { dispatch(setSelectedPage("first")) }, [])
 
   const popularMovies = useAPI(page, "https://api.themoviedb.org/3/movie/popular?")
+  const searchResults = useSearch(page, "https://api.themoviedb.org/3/search/movie?")
 
   return (
     <Container>
-      <Header text={"Popular Movies"} />
+      <Header text={query ? `Search results for "${query}"` : "Popular Movies"} />
       <GridTemplate
-        content={popularMovies}
+        content={query ? searchResults : popularMovies}
         type={"movies"}
       />
       <Pager />
