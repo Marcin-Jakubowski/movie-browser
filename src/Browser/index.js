777f48d08dff0281ce18c1373a4312bb50098589
Pfeszler <pfeszler@gmail.com>
2020-10-14 10:14:31 +0200

    added logic to browser

--- src/Browser/index.js
+++ src/Browser/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import { selectQueryString, setQueryString } from '../Movies/MoviesSlice';
+import useQueryParameter from '../useQueryParameter';
+import useReplaceQueryParameter from '../useReplaceQueryParameter';
 import { BrowserInput } from "./styled"
 
 function Browser() {
+    const dispatch = useDispatch()
+    const location = useLocation()
+    const history = useHistory()
+    const query = useQueryParameter()
+    const queryString = useSelector(selectQueryString)
+
+    if(queryString === "" && query) {
+        dispatch(setQueryString(query))
+    }
+
+    const replaceQueryParameter = useReplaceQueryParameter()
+
+    const onInputChange = ({ target }) => {
+        if (location.pathname.includes("movies" && location.pathname !== "/movies" )) {
+            history.push("/movies")
+        }
+        if (location.pathname.includes("people" && location.pathname !== "/people")) {
+            history.push("/people")
+        }
+        const queryValue = target.value.trim() !== "" ? target.value : undefined
+        replaceQueryParameter({
+            key: "search",
+            value: queryValue
+        });
+        dispatch(setQueryString(queryValue))
+    }
 
     return (
         <BrowserInput
+            value={query || ""}
             placeholder="Search for movies..."
+            onChange={onInputChange}
         />
     );
 };
