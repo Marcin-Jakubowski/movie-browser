a547655dad7bb6fa8f04c6c80b853060a026f95e
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-15 19:10:27 +0200

    adding custom hook to get movie details from API

--- src/MoviePage/index.js
+++ src/MoviePage/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useAPI } from "../Movies/useAPItest";
 import Container from '../Common/Container';
 
 
 function MoviePage() {
+
+    const {id} = useParams();
+    const movieDetails = useAPI(`https://api.themoviedb.org/3/movie/${id}?`);
+    console.log(movieDetails);
+
     return (
         <Container>
             test
--- src/Movies/useAPItest.js
+++ src/Movies/useAPItest.js
@@ -1,28 +1,27 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
-import { apiKey } from "../apiKey"
-import { selectPageInformation, setPageInformation } from "./MoviesSlice"
+import { useDispatch, useSelector } from "react-redux";
+import { apiKey } from "../apiKey";
+import { selectMovieDetails, setMovieDetails } from "./MoviesSlice";
 
 
-export const useAPI = (page, apiLink) => {
+export const useAPI = (apiLink) => {
 
     const dispatch = useDispatch();
-    const data = useSelector(selectPageInformation)
+    const data = useSelector(selectMovieDetails);
 
     const getData = async () => {
         try {
             const response = await axios.get(apiLink, {
                 params: {
-                    api_key: apiKey,
-                    page: page
+                    api_key: apiKey
                 }
             })
-            dispatch(setPageInformation(response.data));
+            dispatch(setMovieDetails(response.data));
         } catch (error) {
             console.log(error)
         }
     };
-    useEffect(() => { getData(page) }, [page]);
-    return data.results;
+    useEffect(() => { getData() }, [apiLink]);
+    return data;
 }
