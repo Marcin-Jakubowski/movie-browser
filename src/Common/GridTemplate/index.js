ce73e95c68da7bd54e9259c7ac5f6f83326dae9d
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-17 00:46:55 +0200

    adding links mapping to grid template component

--- src/Common/GridTemplate/index.js
+++ src/Common/GridTemplate/index.js
@@ -10,7 +10,7 @@ const GridTemplate = ({ content, type }) => {
     >
       {content && content.map((fragment) => (
         <div>
-          {type === "movies" ? <Link to={`/movies/${fragment.id}`}>{fragment.title}</Link> : fragment.name}
+          {type === "movies" ? <Link to={`/movies/${fragment.id}`}>{fragment.title}</Link> : <Link to={`/people/${fragment.id}`}>{fragment.name}</Link>}
         </div>)
       )}
     </Wrapper>
