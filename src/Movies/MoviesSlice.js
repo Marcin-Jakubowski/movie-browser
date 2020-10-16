74c034a3e82e33338048b0c0af0eca9cae469ad5
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-16 20:34:00 +0200

    adding selectors and reducers for movie/personPage

--- src/Movies/MoviesSlice.js
+++ src/Movies/MoviesSlice.js
@@ -10,7 +10,10 @@ const moviesSlice = createSlice({
             total_results: 0,
             total_pages: 0
         },
-        movieDetails: []
+        movieDetails: [],
+        movieCredits: [],
+        personDetails: [],
+        personCredits: []
     },
     reducers: {
         setPageInformation: (state, { payload }) => {
@@ -34,17 +37,32 @@ const moviesSlice = createSlice({
         setMovieDetails: (state, { payload }) => {
             state.movieDetails = payload
         },
+        setMovieCredits: (state, { payload }) => {
+            state.movieCredits = payload
+        },
+        setPersonDetails: (state, { payload }) => {
+            state.personDetails = payload
+        },
+        setPersonCredits: (state, { payload }) => {
+            state.personCredits = payload
+        }
     }
 });
 
 export const {
     setPageInformation,
     setSelectedPage,
-    setMovieDetails
+    setMovieDetails,
+    setMovieCredits,
+    setPersonDetails,
+    setPersonCredits
 } = moviesSlice.actions
 
 export const selectMovies = state => state.movies
 export const selectSelectedPage = state => selectMovies(state).selectedPage
 export const selectPageInformation = state => selectMovies(state).pageInformations
 export const selectMovieDetails = state => selectMovies(state).movieDetails
+export const selectMovieCredits = state => selectMovies(state).movieCredits
+export const selectPersonDetails = state => selectMovies(state).personDetails
+export const selectPersonCredits = state => selectMovies(state).personCredits
 export default moviesSlice.reducer
