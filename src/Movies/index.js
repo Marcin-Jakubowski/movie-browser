aa4fbf1cc05f62f9c7a43c87fb71252d37b43b91
Pfeszler <pfeszler@gmail.com>
2020-10-22 00:21:43 +0200

    Movies display is now dependent on status.

--- src/Movies/index.js
+++ src/Movies/index.js
@@ -3,7 +3,7 @@ import Container from '../Common/Container';
 import GridTemplate from '../Common/GridTemplate';
 import Header from '../Common/Header';
 import { useDispatch, useSelector } from 'react-redux';
-import { initiateFetch, selectPageInformation } from '../MoviesSlice';
+import { initiateFetch, selectPageInformation, selectStatus } from '../MoviesSlice';
 import Pager from '../Common/Pager';
 import useQueryParameter from '../useQueryParameter';
 import { moviesKey, pageKey, searchKey } from '../keys';
@@ -15,26 +15,53 @@ function Movies() {
   const page = useQueryParameter(pageKey)
   const dispatch = useDispatch()
   const movies = useSelector(selectPageInformation)
+  const status = useSelector(selectStatus)
 
 
-  useEffect(() => { dispatch(initiateFetch({
-    page: page ? page : 1,
-    type: type,
-    query: query
-  })) }, [query, page, type, dispatch])
+  useEffect(() => {
+    dispatch(initiateFetch({
+      page: page ? page : 1,
+      type: type,
+      query: query
+    }))
+  }, [query, page, type, dispatch])
 
 
   return (
     <Container>
       <Header text={!query ? `Popular ${type}` : `Search Results for "${query}"`} />
-      <GridTemplate
-        content={movies.results}
-        type={type}
-      />
-      <Pager
-        content={movies}
-        type={type}
-      />
+      {status === "loading" ?
+        <div>
+          loading
+      </div> :
+        ""
+      }
+      {status === "succes" && movies.total_results > 0 ?
+        <GridTemplate
+          content={movies.results}
+          type={type}
+        /> :
+        ""
+      }
+      {status === "succes" && movies.total_results === 0 ?
+        <div>
+          ni mo :(
+       </div> :
+        ""
+      }
+      {status === "failed" ?
+        <div>
+          failed
+      </div> :
+        ""
+      }
+      { status === "succes" && movies.total_results > 0 ?
+        <Pager
+          content={movies}
+          type={type}
+        /> :
+        ""
+      }
     </Container>
   );
 };
