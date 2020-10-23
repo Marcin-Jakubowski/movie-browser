59a3bf37532c9b8a8a053a7e3d8609e4082d5f82
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-24 01:08:27 +0200

    adding genres list to GridTemplate component

--- src/Common/GridTemplate/index.js
+++ src/Common/GridTemplate/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { moviesKey } from "../../keys";
 import MovieSmallTile from "../../MovieSmallTile";
 import { Wrapper, GridTemplateLink } from "./styled";
-
+import {  selectGenres, setGenres } from "../../MoviesSlice";
 
 const GridTemplate = ({ content, type }) => {
+  const dispatch = useDispatch();
+  const genresData = useSelector(selectGenres);
+
+  if (genresData === []) {
+    dispatch(setGenres([]))
+  } else {};
+
   return (
     <Wrapper
       type={type}
@@ -12,14 +20,12 @@ const GridTemplate = ({ content, type }) => {
       {content && content.map((fragment) => (
         <div>
           {type === moviesKey
-          ? <GridTemplateLink to={`/movies/${fragment.id}`}>{<MovieSmallTile fragment={fragment} />}</GridTemplateLink>
+          ? <GridTemplateLink to={`/movies/${fragment.id}`}>{<MovieSmallTile content={fragment} />}</GridTemplateLink>
           : <GridTemplateLink to={`/people/${fragment.id}`}>{fragment.name}</GridTemplateLink>}
         </div>)
       )}
     </Wrapper>
   )
 }
-export default GridTemplate
 
-
-
+export default GridTemplate;
