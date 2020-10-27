bcff693e61261a303467f87cf079c3166a169203
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-27 22:54:40 +0100

    restyling browser component

--- src/Browser/styled.js
+++ src/Browser/styled.js
@@ -2,13 +2,18 @@ import styled from "styled-components";
 
 export const BrowserInput = styled.input`
     line-height: 24px;
+    font-size: 16px;
     padding: 12px;
     border-radius: 24px;
     width: 432px;
-    max-height: 44px;
+    max-height: 48px;
     justify-self: end;
 
-@media( max-width: 767px) {
-    width: 100%;
-}
+    @media( max-width: 767px) {
+        width: 100%;
+        line-height: 17px;
+        font-size: 13px;
+        max-height: 44px;
+        border-radius: 22px;
+    }
 `;
