ae412e3eda37bb4c887f7699e53c739629253e64
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-10-28 00:42:44 +0100

    restyling Container

--- src/Common/Container/styled.js
+++ src/Common/Container/styled.js
@@ -6,7 +6,7 @@ export const Main = styled.main`
 
     @media( max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
           max-width: unset;
-          width: calc(100% - 16px);
+          width: calc(100% - 32px);
           margin: 24px 16px;
         }
 `
