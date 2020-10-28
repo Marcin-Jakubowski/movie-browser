3556c31726c7612c1f91367ade9351e07f1034bc
Pfeszler <pfeszler@gmail.com>
2020-10-28 12:22:35 +0100

    added margin to container when width < 1400px

--- src/Common/Container/styled.js
+++ src/Common/Container/styled.js
@@ -4,6 +4,11 @@ export const Main = styled.main`
     max-width: 1368px;
     margin: 56px auto;
 
+    @media(max-width: ${({ theme }) => theme.breakpoint.noContainer}px) {
+        margin: 56px 20px
+
+    }
+
     @media( max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
           max-width: unset;
           width: calc(100% - 32px);
